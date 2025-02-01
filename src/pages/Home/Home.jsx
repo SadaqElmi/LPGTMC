3b28ce78bdc3634330768c675aa7be0b6ffb8b97
TaariqDeveloper <TaariqDeveloper@gmail.com>
2025-02-01 22:16:30 +0300

    added Bloging page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,14 +1,12 @@
 import React from "react";
 import Herosection from "./components/Hero";
 import FeaturedCars from "./components/FeaturedCars";
-// import Blogs from "../Blogs/Components/Blogs";
 
 const Home = () => {
   return (
     <div>
       <Herosection />
       <FeaturedCars />
-      {/* <Blogs /> */}
     </div>
   );
 };
