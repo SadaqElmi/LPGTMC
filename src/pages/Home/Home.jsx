0d6c90b04bba4bdf4ca2bf7ae43a54f375e791d2
Ashwaaq ibraahiim <157350733+Ashwaaq69@users.noreply.github.com>
2025-02-01 11:57:45 +0300

    created hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import Herosection from "../../components/Hero";
+import Herosection from "./components/Hero";
+import FeaturedCars from "./components/FeaturedCars";
 
 
 const Home = () => {
   return (
     <div>
       <Herosection />
+      <FeaturedCars />
     </div>
   )
   
