35e9d3b4d66261d07ec2b7f7f5e66c9e78593aa1
SadaqElmi <hooyomacaansadaq@gmail.com>
2025-02-06 20:39:49 +0300

    FIxed

--- src/pages/Cars/Cars.jsx
+++ src/pages/Cars/Cars.jsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import ExploreCars from './components/ExploreCars.jsx';
+import ExploreCars from "./components/ExploreCars.jsx";
 
 const Cars = () => {
   return (
     <div>
       <div className="py-4 m-auto max-w-7xl">
         <p className="text-gray-600 ml-4">
-          <NavLink to="/" className="text-blue-500 hover:text-blue-300">Home</NavLink> » Explore cars
+          <NavLink to="/" className="text-blue-500 hover:text-blue-300">
+            Home
+          </NavLink>{" "}
+          » Explore cars
         </p>
         <ExploreCars />
       </div>
